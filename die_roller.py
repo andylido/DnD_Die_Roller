695de77492ff3baaf483c8b15aa3f29cefe1cfed
Andy Li <ali37@uic.edu>
2019-11-13 12:09:46 -0600

    Updated Program to work with modification numbers

--- die_roller.py
+++ die_roller.py
@@ -1,6 +1,8 @@
+
 def roll_die(side):
     import random
     return random.randint(1,side)
+
 
 def roll_record(side, times):
     L = []
@@ -10,43 +12,83 @@ def roll_record(side, times):
         i += 1
     return L
 
-def result_display(results, side):
-    total = sum(results)
-    print('   Roll a %d sided die, %d times.' %(side, len(results)))
-    print('\n   Your rows are',results,'for a total of %d\n' %(total))
-    
 
+def result_display(results, side, mod):
+    if mod == 0:
+        total = sum(results)
+        print('         %d sided die, %d time(s)' %(side, len(results)))
+        print('         Your row(s) are',results,'for a total of %d\n' %(total))
+    else:
+        total = sum(results) + mod
+        print('         %d sided die, %d time(s), mod %d.' %(side, len(results), mod))
+        print('         Your row(s) are',results,'with mod %d for a total of %d\n' %(mod,total))
+
+        
 def user_input():
     valid_input = False
     while valid_input == False:
-        user = input('\n   Roll: ')
-        if 'd' in user:
-            L = user.split('d')
-            times = L[0]
-            side = L[1]
-
+        user = input('   Roll: ')
+        if user == 'q':
+            print('\n   Program Terminated.\n')
+            return
+        elif 'd' in user:
             if '+' in user or '-' in user:
                 if '+' in user:
-                    M = user.split('+')
+                    if len(user.split('+'))<3:
+                        mod_split = user.split('+')
+                        L = mod_split[0].split('d')
+                        times = L[0]
+                        side = L[1]
+                        M = user.split('+')
+                        mod = M[1]
+                        try:
+                            times = int(times)
+                            side = int(side)
+                            mod = int(mod)
+                            results = roll_record(side,times) #number of intervals, digits per interval
+                            result_display(results, side, mod)
+                        except ValueError:
+                            print("   Error in input.")
+                    else:
+                        print("   Error in input")
                 elif '-' in user:
-                    M = user.split('-')
+                    if len(user.split('-'))<3:
+                        mod_split = user.split('-')
+                        L = mod_split[0].split('d')
+                        times = L[0]
+                        side = L[1]
+                        M = user.split('-')
+                        mod = M[1]
+                        try:
+                            times = int(times)
+                            side = int(side)
+                            mod = 0-int(mod)
+                            results = roll_record(side,times) #number of intervals, digits per interval
+                            result_display(results, side, mod)
+                        except ValueError:
+                            print("   Error in input")
+                    else:
+                        print("   Error in input")
                 else:
-                    print('Error Detected.\n')
+                    print("   Error in input")
                 
             else:
+                L = user.split('d')
+                times = L[0]
+                side = L[1]
+                mod = 0
                 try:
                     times = int(times)
                     side = int(side)
-                    valid_input = True
+                    results = roll_record(side,times) #number of intervals, digits per interval
+                    result_display(results, side, mod)
                 except ValueError:
-                    print("   Error. Input must be integers.\n")
+                    print("    Please input (#of times rolled)d(#of dice sides) or 'q' to quit.")
         else:
-            print('\n   Please input (#of times rolled)d(#of dice sides). \n')
+            print("     Please input (#of times rolled)d(#of dice sides) or 'q' to quit.")
             valid_input == False
-            
-    results = roll_record(side,times) #number of intervals, digits per interval
-    result_display(results, side)
 
+            
 def program_IO():
     print("\n   Die Roller (v1.0)")
     print("   Directions: Enter #d# to roll.\n   EXAMPLE: 4d6 Rolls a 6 SIDED die 4 TIMES.\n")
